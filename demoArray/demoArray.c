a7a1651dd2feb133d2db608e46322c1fac4b3423
yywjk-dmn <2986567641@qq.com>
2023-12-08 15:58:07 +0800

    [新增]新增数组的定义和初始化

---
+++ demoArray/demoArray.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 10
+
+/* 一段连续的存储空间 存放的是相同的数据类型*/
+int main()
+{
+    /* 方法一 ：使用定义即初始化 */
+    int array[BUFFER_SIZE] = {1, 2, 3};   //后面的值不写默认为0
+
+    for (int idx = 0; idx < BUFFER_SIZE; idx++)
+    {
+        printf("array[%d]: %d\n",idx, array[idx]);
+    }
+    int len = sizeof(array);
+    printf("len:%d\n", len);
+
+    /* 地址 */
+    printf("%p\n", &array[0]);
+    printf("%p\n", &array[1]);
+    printf("%p\n", &array[9]);
+    /* 数组的（首）地址和数组首元素的地址是一个*/
+
+    /* 清除脏数据 */
+    memset(array, 0, sizeof(array));
+    printf("------------\n");
+     for (int idx = 0; idx < BUFFER_SIZE; idx++)
+    {
+        printf("array[%d]: %d\n",idx, array[idx]);
+    }
+    return 0;
+}
