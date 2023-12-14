cd2b65914eff3e37b16009164211503ba789f508
yywjk-dmn <2986567641@qq.com>
2023-12-15 00:24:18 +0800

    [新增]新增Makefile

--- demoMakefile/main.c
+++ demoMakefile/main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int printString()
-{
-    int ret;
-    printf("print string\n");
-    return ret;
-}
 int main()
 {
-    int num1 = 10;
-    printf("num1:%d\n",num1);
-    printString();
-
+    printf("hello world\n");
     return 0;
 }
