b577cae56ff5fdb2ae06be43557e003386a8581b
yywjk-dmn <2986567641@qq.com>
2023-12-15 11:34:09 +0800

    [新增]新增数组做函数参数

--- pointer/demoFuncVara.c
+++ pointer/demoFuncVara.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 
-
+#define BUFFER_SIZE 10
+#define BUFFER_OFFSET 97
 
 /* 函数 */
 
@@ -10,6 +11,19 @@
 /* 函数参数 */
   /* 需求：实现两个函数的交换 */
   /* 值传递 */
+
+
+/* 数组做函数参数会自动弱化为指针 */
+void printArrary(int *array, int numSize)
+{
+    //int elementNum = sizeof(array) / sizeof(array[0]);  //求空间的大小
+    for (int idx = 0;idx < numSize; idx++)
+    {
+        printf("array[%d] = %d\t", idx, array[idx]);
+    }
+    return;
+
+}
 
 int  swapRealDate(int *val1, int *val2)
 {
@@ -53,18 +67,32 @@ int main()
 
     //swapSurfaceDate(num1, num2);     //为什么值没有进行交换
 
-
+#if 0
     int a= 5;
     int *p = &a;
 
     *p = 200;
     printf("a = %d\n", a);
     swapRealDate(&num1, &num2);
-
-    
     printf("num1:%d, num2:%d\n", num1, num2);
+#endif
 
+    int array[BUFFER_SIZE];
+    memset(array, 0, sizeof(array));
+    int elementNum = sizeof(array) / sizeof(array[0]);  //求空间的大小
+    printf("len:%d\n", elementNum);
+    for (int idx = 0;idx < elementNum; idx++)
+    {
+        array[idx] = idx + BUFFER_OFFSET;
+    }
+    printArrary(array, elementNum);
+    /* 数组名就是数组的首地址 也是数组第一个元素的首地址 */
+    // for (int idx = 0;idx < elementNum; idx++)
+    // {
+    //     printf("array[%d] = %d\t",idx, array[idx]);
 
+    // }
+    printf("\n");
 
     return 0;
 }
